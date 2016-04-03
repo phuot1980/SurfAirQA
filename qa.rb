0b05010b7552d36a0517b0f2f9dc0aa68f001990
ponlok huot <phuot1980@gmail.com>
2016-04-03 12:47:27 -0700

    finshed rspec testing with 11 examples

--- qa.rb
+++ qa.rb
@@ -21,8 +21,9 @@ describe 'Surfair qa' do
 			element_username = @driver.find_element(:id, 'input_0').send_keys ""
 			element_password = @driver.find_element(:id, 'input_1').send_keys ""
 			@driver.find_element(:class, 'button').click
+			# expect it to change to home page
+			expect(@driver.current_url).to eql("https://staging.surfair.com/app/#/")
 		end
-		# expect it to change to home page 
 	end
 
 	describe 'book a flight' do 
@@ -34,6 +35,7 @@ describe 'Surfair qa' do
 				element_book.click
 			end
 			# expect it to click a book a flight link
+			expect(@driver.current_url).to eql("https://staging.surfair.com/app/#/book")
 		end
 
 		it 'chooses a Hawthorne airport for departure' do 
@@ -43,7 +45,8 @@ describe 'Surfair qa' do
 				element_depart = @driver.find_element(:partial_link_text, 'Hawthorne')
 				element_depart.click
 			end
-		# expect it to chose Hawthorne airport
+		# expect it to chose Hawthorne airport	
+		expect(@driver.current_url).to include("HHR")
 		end
 
 		it 'chooses Oakland airport for arrival' do 
@@ -53,8 +56,9 @@ describe 'Surfair qa' do
 				element_arrival = @driver.find_element(:partial_link_text, 'Oakland')
 				element_arrival.click
 			end
-		end
 		#expect it to chose Oakland airport
+		expect(@driver.current_url).to include("OAK")	
+		end
 	end
 
 	describe 'chose a date for travel' do 
@@ -65,8 +69,18 @@ describe 'Surfair qa' do
 				element_next_month = @driver.find_element(:class, 'next')
 				element_next_month.click
 			end
+		# expect the calendar to move forward a month	
+		element_current_month = @driver.find_elements(:class, 'month')
+		element_current_month.each do |m|
+			 if m.text() == m.text()
+			 	element_next_month.click
+			 else
+			 	puts "this month is #{m.text()}"
+			 end
 		end
-		# expect the calendar to move forward a month
+		expect(@driver.current_url).to eql('https://staging.surfair.com/app/#/book?from=HHR&to=OAK')		
+		end
+
 
 		it 'chooses day 13' do 
 			begin
@@ -75,8 +89,8 @@ describe 'Surfair qa' do
 				element_day = @driver.find_element(:link_text, '13')
 				element_day.click
 			end
+			expect(@driver.current_url).to include("13")
 		end
-		# expect the 13 day to be picked
 
 		it 'books the first available flight' do 
 			begin
@@ -85,8 +99,9 @@ describe 'Surfair qa' do
 				element_book = @driver.find_element(:class, 'button-book')
 				element_book.click
 			end
+			# expect flight to be booked
+			expect(@driver.current_url).to eql("https://staging.surfair.com/app/#/book?from=HHR&to=OAK&day=2016-05-13")
 		end
-		# expect flight to be booked
 	end
 
 	describe 'canceling the flight' do
@@ -99,16 +114,14 @@ describe 'Surfair qa' do
 				element_manage_flight = @driver.find_element(:link, 'Manage Flights')
 				element_manage_flight.click
 			end
+			expect(@driver.current_url).to eql("https://staging.surfair.com/app/#/")
 		end
-		# expect to see all booked flights
 
 		it 'cancels the flight' do
 			begin
-				# sleep 5
 				element_more = @driver.find_element(:class, 'fa-chevron-down')
 				element_more.click
 			ensure 
-				# sleep 5
 				element_cancel = @driver.find_elements(:class, 'button')
 				element_cancel.each do |t|
 					if t.text() == "Cancel This Flight"
@@ -119,6 +132,7 @@ describe 'Surfair qa' do
 					end
 				end
 			end
+			expect(@driver.current_url).to eql("https://staging.surfair.com/app/#/")
 		end
 
 		it 'confirm flight cancel' do
@@ -126,12 +140,14 @@ describe 'Surfair qa' do
 			element_cancel.each do |t|
 				if t.text() == 'Cancel Flight'
 					t.click
-					puts "Found the Confirm cancel buton"
+					puts t.text() && "Found the Confirm cancel buton"
 				else
 					puts t.text()
 				end
 			end
+			expect(@driver.current_url).to eql("https://staging.surfair.com/app/#/")
 		end
+	
 	end
 
 	describe 'logout' do 
@@ -146,6 +162,7 @@ describe 'Surfair qa' do
 				# @driver.quit
 				puts "logged out"
 			end
+			expect(@driver.current_url).to eql("https://staging.surfair.com/app/#/profile")
 		end
 	end
 
